8cdbdbe3422ffefb9d5e458954a8303b2f49dcec
Charlie Griffin <charliegriffin@email.arizona.edu>
2018-04-24 07:47:08 -0500

    render AR cube

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Charles Griffin. All rights reserved.
 //
 
+//test
 import UIKit
 import SceneKit
 import ARKit
@@ -20,11 +21,29 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        let material = SCNMaterial()
+        
+        material.diffuse.contents = UIColor.red
+        
+        cube.materials = [material]
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        
+        node.geometry = cube
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
+        
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
